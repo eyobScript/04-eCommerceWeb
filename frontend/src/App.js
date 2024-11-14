b90641f65fb436341af947052df289c7c5a11ac8
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-14 10:16:18 +0300

    we add home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Container } from 'react-bootstrap'
 import Header from "./Component/Header/Header";
 import Footer from './Component/Footer/Footer'
+import HomeScreen from "./screen/HomeScreen";
+
 function App() {
   return (
     <>
     <Header />
-    <main>
+    <main className="py-3">
       <Container>
-        <h1>welcome to eShop</h1>
+        <HomeScreen />
       </Container>
     </main>
     <Footer />
