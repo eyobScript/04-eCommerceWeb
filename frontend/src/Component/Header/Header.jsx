09f3fdb19c2258a8bf1075f315c19c1f2ec21c3d
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-14 10:16:51 +0300

    header

--- frontend/src/Component/Header/Header.jsx
+++ frontend/src/Component/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Navbar,Nav, Container } from 'react-bootstrap'
 
 function Header() {
   return (
     <header>
-      <h1>this Header</h1>
+    <Navbar bg="dark" variant='dark' expand='lg' collapseOnSelect>
+      <Container >
+          <Navbar.Brand href="/">eShope</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse  className="justify-content-end" id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart"> <i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+              <Nav.Link href="/signIn"> <i className='fas fa-user'></i> SignIn</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+    </Navbar>
     </header>
   )
 }
