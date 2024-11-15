4a6b0c7d07580550b62bb65025c9775567a21fc1
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-15 11:02:03 +0300

    home screen

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -9,8 +9,16 @@ function HomeScreen() {
       <h1>Latest Product</h1>
       <Row>
       {
-        products.map(product => <Col sm={12} md={6} lg={4} xl={3}>
-          <Product product={product} />
+        products.map(product => 
+        <Col 
+        key={product._id}
+        sm={12} 
+        md={6} 
+        lg={4} 
+        xl={3}>
+        <Product 
+        product={product} 
+        />
         </Col>
         )
       }
