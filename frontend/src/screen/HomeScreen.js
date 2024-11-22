0f91626f49e9403e1244e4eb7fccc0f6d7800cb3
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-22 10:13:44 +0300

    home screen

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import products from '../products'
-import { Row, Col } from 'react-bootstrap';
-import Product from '../Component/Product/Product';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from "../Component/Product/Product";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProduct] = useState([]);
+  useEffect(() => {
+    async function fetchProduct() {
+      const { data } = await axios.get("/api/products");
+
+      setProduct(data);
+    }
+    fetchProduct();
+  }, []);
+
   return (
     <>
       <h1>Latest Product</h1>
       <Row>
-      {
-        products.map(product => 
-        <Col 
-        key={product._id}
-        sm={12} 
-        md={6} 
-        lg={4} 
-        xl={3}>
-        <Product 
-        product={product} 
-        />
-        </Col>
-        )
-      }
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
       </Row>
     </>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
