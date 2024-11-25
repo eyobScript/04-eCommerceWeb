3c5c8780dba5c98ab31dd026c9bb3afc879c55e3
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-25 20:54:38 +0300

    updated

---
+++ backend/data/users.js
@@ -0,0 +1,21 @@
+
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: "password123",
+    isAdmin: true,
+  },
+  {
+    name: "Job Manager",
+    email: "job@example.com",
+    password: "password123",
+  },
+  {
+    name: "john Doe",
+    email: "john@example.com",
+    password: "password123",
+  },
+]; 
+ 
