829ff38845bd1cb10a5afacdb65dc31ff8f32e6c
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-26 19:05:47 +0300

    error handle

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,27 @@
-import express from 'express';
-import colors from 'colors'; 
-import products from './data/products.js';
-import connectDB from './config/db.js'
+import express from "express";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-dotenv.config();
+import productRoutes from "./routes/productRoute.js";
+import { errorHandler, notFound} from './middleware/errorMiddleware.js'
 
+dotenv.config();
 
 connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
+app.get("/", (req, res) => {
+  res.send("API is running....");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-    if (product) {
-        res.json(product);
-    } else {
-        res.status(404).json({ message: 'Product not found' });
-    }
-});
 
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => {
-    console.log(`Server is running ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
+  console.log(
+    `Server is running ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold
+  );
 });
