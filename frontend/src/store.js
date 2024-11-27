c9a0da2d07c7da9cf80ad9200ff767ee923219d6
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-27 18:36:12 +0300

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux'
+import { legacy_createStore as createStore , combineReducers } from 'redux';
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension' 
 
