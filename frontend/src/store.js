9db6bceee7bb994fb353c21c43f5033bdbce345d
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-27 18:40:13 +0300

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,9 @@
-import { legacy_createStore as createStore , combineReducers } from 'redux';
+import { legacy_createStore as createStore , combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension' 
 
 const reducer = combineReducers({});
 
-const store = createStore(reducer)
+const initialState = {}
+
+const store = createStore(reducer, initialState)
