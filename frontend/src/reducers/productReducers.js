b93a906766fd7c04ddb0d5f5003e71dbde10d8c7
eyobScript <eyobbmulugeta@gmail.com>
2024-11-28 16:37:48 +0300

    WE IMPORT TO OUR PRODUCT REDUCER

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,12 +1,14 @@
+import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST } from '../constants/productConstants'
+
 export function productListReducer(state = { product: [] }, action) {
   // Implement for product reducer
 
   switch (action.type) {
-    case "PRODUCT_LIST_REQUEST":
+    case PRODUCT_LIST_REQUEST:
       return { loading: true };
-    case "PRODUCT_LIST_SUCCESS":
+    case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-    case "PRODUCT_LIST_FAILURE":
+    case PRODUCT_LIST_FAILURE:
       return { loading: false, error: action.payload };
     default:
       return state;
