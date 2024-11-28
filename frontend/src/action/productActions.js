fdf4c9f216cd7ae9177528bfe826ab060a7c7423
eyobScript <eyobbmulugeta@gmail.com>
2024-11-28 18:18:37 +0300

    this action of production

--- frontend/src/action/productActions.js
+++ frontend/src/action/productActions.js
@@ -1,5 +1,18 @@
+import axios from 'axios'
 import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from "../constants/productConstants";
+
+export const listProduct = async (dispatch) => {
+    try {
+        dispatch({ typ: PRODUCT_LIST_REQUEST});
+        
+        const { data } = await axios.request('/products/api');
+
+        dispatch( { type: PRODUCT_LIST_SUCCESS});
+    } catch (error) {
+        
+    }
+}
