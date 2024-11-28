cf25d1cbca2f9de36fe721d0c410ca53e619ec1c
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-28 16:03:27 +0300

    basic case for switch

--- frontend/reducers/productReducers.js
+++ frontend/reducers/productReducers.js
@@ -1,3 +1,14 @@
-export function productReducer(state = {}, action ) {
-    // Implement for product reducer 
+export function productReducer(state = { product: [] }, action) {
+  // Implement for product reducer
+
+  switch (action.type) {
+    case "PRODUCT_LIST_REQUEST":
+      return { loading: true };
+    case "PRODUCT_LIST_SUCCESS":
+      return { loading: false, products: action.payload };
+    case "PRODUCT_LIST_FAILURE":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
 }
