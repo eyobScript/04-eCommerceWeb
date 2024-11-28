347796f1c38b4a1f2f4da32019fcaf0eca4e4a6f
eyobScript <eyobbmulugeta@gmaol.com>
2024-11-28 16:07:19 +0300

    store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import {
 } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import {} from './'
+import {} from './reducers/productReducers'
 
 const reducer = combineReducers({});
 
