e7e1d9dc95f3eeef299ecef2dfde3470eef99ed0
eyobScript <eyobbmulugeta@gmail.com>
2024-11-28 16:39:09 +0300

    updated a lit bit

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,8 @@
-import { PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST } from '../constants/productConstants'
+import {
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL,
+  PRODUCT_LIST_REQUEST,
+} from "../constants/productConstants";
 
 export function productListReducer(state = { product: [] }, action) {
   // Implement for product reducer
@@ -8,7 +12,7 @@ export function productListReducer(state = { product: [] }, action) {
       return { loading: true };
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
-    case PRODUCT_LIST_FAILURE:
+    case PRODUCT_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
